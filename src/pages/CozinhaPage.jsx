588bf72d3ce8a3ebe2cbb6a2ab598d1a57ec6011
m4arthu <luisarthir259@gmail.com>
2023-12-04 18:36:49 -0300

    feat: loading component

--- src/pages/CozinhaPage.jsx
+++ src/pages/CozinhaPage.jsx
@@ -4,9 +4,10 @@ import { HeaderComponent } from "../components/HeaderComponent.jsx"
 import { OrderUsername } from "../components/OrderUsername.jsx"
 import { useEffect, useState } from "react"
 import { getOrders } from "../services/orders.service.js"
-export function CozinhaPage({ onlyNames,headerNumber }) {
-    const [orders,setOrders] = useState([])
-    useEffect(()=>{
+import { LoadingComponent } from "../components/loadinfComponent.jsx"
+export function CozinhaPage({ onlyNames, headerNumber }) {
+    const [orders, setOrders] = useState([])
+    useEffect(() => {
         const fetchOrders = async () => {
             try {
                 const ordersData = await getOrders();
@@ -17,52 +18,57 @@ export function CozinhaPage({ onlyNames,headerNumber }) {
             }
         };
         fetchOrders();
-    },[])
-    if (!onlyNames) {
-        return (
-            <>
-                <HeaderComponent selectedButtonId={2} />
-                <CozinhaContainer >
-                    <CozinhaContent>
-                        <div className="orders">
-                            <h1>Preparando:</h1>
-                            {orders? orders.filter(order => order.isFinished === false).map(order =>{
-                                return <OrderComponent orderId={order.id}  key={order.id} data={order} />
-                            }):""}
-                        </div>
-                        <div className="division"></div>
-                        <div className="orders">
-                            <h1>Pronto:</h1>
-                            {orders? orders.filter(order => order.isFinished === true).map(order =>{
-                                return <OrderComponent   key={order.id} selected={true} data={order} />
-                            }):""}
-                        </div>
-                    </CozinhaContent>
-                </CozinhaContainer>
-            </>
-        )
-    } else {
-        return (
-            <>
-             <HeaderComponent selectedButtonId={headerNumber} />
-                <CozinhaContainer >
-                    <CozinhaContent>
-                        <div className="orders">
-                            <h1>Preparando:</h1>
-                            {orders? orders.filter(order => order.isFinished === false).map(order =>{
-                                return <OrderUsername   key={order.id} name={order.username} />
-                            }):""}
-                        </div>
-                        <div className="division"></div>
-                        <div className="orders">
-                            <h1>Pronto:</h1>
-                            {orders? orders.filter(order => order.isFinished === true).map(order =>{
-                                return <OrderUsername key={order.id}  finished={true} name={order.username} />
-                            }):""}
-                        </div>
-                    </CozinhaContent>
-                </CozinhaContainer>
-            </>
-        )
+    }, [])
+    if (orders.length > 0) {
+
+        if (!onlyNames) {
+            return (
+                <>
+                    <HeaderComponent selectedButtonId={2} />
+                    <CozinhaContainer >
+                        <CozinhaContent>
+                            <div className="orders">
+                                <h1>Preparando:</h1>
+                                {orders ? orders.filter(order => order.isFinished === false).map(order => {
+                                    return <OrderComponent orderId={order.id} key={order.id} data={order} />
+                                }) : ""}
+                            </div>
+                            <div className="division"></div>
+                            <div className="orders">
+                                <h1>Pronto:</h1>
+                                {orders ? orders.filter(order => order.isFinished === true).map(order => {
+                                    return <OrderComponent key={order.id} selected={true} data={order} />
+                                }) : ""}
+                            </div>
+                        </CozinhaContent>
+                    </CozinhaContainer>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <HeaderComponent selectedButtonId={headerNumber} />
+                    <CozinhaContainer >
+                        <CozinhaContent>
+                            <div className="orders">
+                                <h1>Preparando:</h1>
+                                {orders ? orders.filter(order => order.isFinished === false).map(order => {
+                                    return <OrderUsername key={order.id} name={order.username} />
+                                }) : ""}
+                            </div>
+                            <div className="division"></div>
+                            <div className="orders">
+                                <h1>Pronto:</h1>
+                                {orders ? orders.filter(order => order.isFinished === true).map(order => {
+                                    return <OrderUsername key={order.id} finished={true} name={order.username} />
+                                }) : ""}
+                            </div>
+                        </CozinhaContent>
+                    </CozinhaContainer>
+                </>
+            )
+        }
+    } else{
+        return <LoadingComponent/>
     }
 }
---
+++ src/styles.css/loading.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+export const  LoadingContainer = styled.div`
+width:100vw;
+margin:20px;
+
+`
